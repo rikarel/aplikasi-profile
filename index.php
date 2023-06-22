301959e9aabedc284e14e0708d041f49f80e09ae
rikarel <relitaa87@gmail.com>
2023-06-22 20:16:16 +0700

    satu

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profil Saya</title>
+    <link rel="stylesheet" type="text/css" href="assets/styles/style.css">
+</head>
+<body>
+    <header class="one">
+        <h1>Profil Saya</h1>
+    </header>
+
+    <article>
+        <header class="two">
+            <h2>About Me</h2>
+            <figure>
+                <img src="https://storage.cloud.google.com/project-rikarelita-567890/myfoto.jpeg" height="300px" width="300px">
+            </figure>
+        </header>
+
+        <section>
+            <table>
+                <tr>
+                    <th>Nama</th>
+                    <td>Rika Relitaa</td>
+                </tr>
+                <tr>
+                    <th>Tempat Lahir</th>
+                    <td>Bantul</td>
+                </tr>
+                <tr>
+                    <th>Universitas</th>
+                    <td>Universitas Nahdlatul Ulama Yogyakarta</td>
+                </tr>
+                <tr>
+                    <th>Prodi</th>
+                    <td>Informatika</td>
+                </tr>
+                <tr>
+                    <th>Path</th>
+                    <td>Cloud Computing</td>
+                </tr>
+            </table>
+        </section>
+    </article>
+
+</body>
+</html>
